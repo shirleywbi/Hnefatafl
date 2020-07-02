ba295e052342bec810471f41a512e757dba076b6
shirleywbi <swwbi@msn.com>
2020-07-01 20:51:09 -0700

    add game

--- app/src/main/java/com/shirleywbi/hnefatafl/Game.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/Game.kt
@@ -1,4 +1,19 @@
 package com.shirleywbi.hnefatafl
 
+import com.shirleywbi.hnefatafl.pieces.PlayerType
+
 class Game {
+
+    var gameBoard: Board = Board()
+    lateinit var player: PlayerType
+    var isAttackerTurn: Boolean = true
+
+    fun selectPlayer(type: PlayerType) : Unit {
+        player = type
+    }
+
+    fun nextTurn() : Unit {
+        isAttackerTurn = !isAttackerTurn
+    }
+
 }
