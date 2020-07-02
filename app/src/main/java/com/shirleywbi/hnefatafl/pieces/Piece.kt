6bde6c27367ae2b946213a69b5af9ccf5cc9b793
shirleywbi <swwbi@msn.com>
2020-07-01 23:13:01 -0700

    add piece capture

--- app/src/main/java/com/shirleywbi/hnefatafl/pieces/Piece.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/pieces/Piece.kt
@@ -30,10 +30,31 @@ abstract class Piece(var x: Int, var y: Int, var type: PlayerType) {
         y = newY
     }
 
-    // TODO: Determine how pieces are captured
-    fun getCaptured() : Unit {
+    fun getCaptured() {
         x = -1
         y = -1
+    }
+
+
+    fun capture(x: Int, y: Int, layoutMap: HashMap<Pair<Int, Int>, Piece>, player: PlayerType) {
+        singleCapture(x, y, 1, 0, layoutMap, player)
+        singleCapture(x, y, -1, 0, layoutMap, player)
+        singleCapture(x, y, 0, 1, layoutMap, player)
+        singleCapture(x, y, 0, -1, layoutMap, player)
+    }
+
+    // Capture if piece is surrounded vertically or horizontally by a piece or restricted spot
+    private fun singleCapture(x: Int, y: Int, xOffset: Int, yOffset: Int, layoutMap: HashMap<Pair<Int, Int>, Piece>, player: PlayerType) {
+        if (layoutMap[Pair(x + xOffset, y + yOffset)]?.type != player &&
+            (layoutMap[Pair(x + xOffset * 2, y + yOffset * 2)]?.type == player ||
+            inRestricted(x + xOffset * 2, y + yOffset * 2))) {
+            layoutMap.remove(Pair(x + xOffset, y + yOffset))
+        }
+    }
+
+    // Returns true if position is in a board corner or center
+    private fun inRestricted(x: Int, y: Int) : Boolean {
+        return (x == 0 && y == 0) || (x == 10 && y == 0) || (x == 0 && y == 10) || (x == 10 && y == 10) || (x == 5 && y == 5)
     }
 
 }
