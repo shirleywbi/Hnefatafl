244437320b712f79bac906e674ed550b539f04ac
shirleywbi <swwbi@msn.com>
2020-07-03 00:44:35 -0700

    add dps conversion

--- app/src/main/java/com/shirleywbi/hnefatafl/util/UIUtils.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/util/UIUtils.kt
@@ -1,6 +1,9 @@
-package com.shirleywbi.hnefatafl.ui
+package com.shirleywbi.hnefatafl.util
 
-fun getDps(): Float {
-    return 1f
+import android.view.View
+
+fun getDps(dps: Int, view: View): Int {
+    val scale = view.context.resources.displayMetrics.density
+    return (dps * scale + 0.5f).toInt()
 }
 
