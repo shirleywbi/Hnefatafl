c517633f5e67f6a3cfb92bb937cd3274eed4173f
shirleywbi <swwbi@msn.com>
2020-07-03 23:52:22 -0700

    add player selection button listeners

--- app/src/main/java/com/shirleywbi/hnefatafl/MainActivity.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/MainActivity.kt
@@ -2,11 +2,28 @@ package com.shirleywbi.hnefatafl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shirleywbi.hnefatafl.service.Game
+import com.shirleywbi.hnefatafl.service.pieces.PlayerType
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var game = Game()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setPlayerSelectListeners()
     }
+
+    fun setPlayerSelectListeners() {
+        attacker_btn.setOnClickListener {
+            game.selectPlayer(PlayerType.ATTACKER)
+        }
+        defender_btn.setOnClickListener {
+            game.selectPlayer(PlayerType.DEFENDER)
+        }
+    }
+
 }
