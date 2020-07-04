679eb64732f61f006a6125f6108be8d7f81ad212
shirleywbi <swwbi@msn.com>
2020-07-04 01:47:03 -0700

    implement add pieces in board view

--- app/src/main/java/com/shirleywbi/hnefatafl/ui/BoardView.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/ui/BoardView.kt
@@ -3,9 +3,16 @@ package com.shirleywbi.hnefatafl.ui
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.shirleywbi.hnefatafl.service.pieces.Piece
+import com.shirleywbi.hnefatafl.service.pieces.PlayerType
+import com.shirleywbi.hnefatafl.ui.pieces.AttackerPieceView
+import com.shirleywbi.hnefatafl.ui.pieces.DefenderPieceView
+import com.shirleywbi.hnefatafl.ui.pieces.PieceView
 import com.shirleywbi.hnefatafl.util.getDps
 
 class BoardView : ConstraintLayout {
+
+    private val size: Int = getDps(400, this) / 11
 
     constructor(context: Context) : this(context, null)
 
@@ -14,10 +21,11 @@ class BoardView : ConstraintLayout {
     }
 
     private fun initialize(context: Context, attrs: AttributeSet?) {
-        var cells: ArrayList<BoardCellView> = arrayListOf()
+        addCells()
+//        addPieces() // TODO: Load layout map
+    }
 
-        val size: Int = getDps(400, this) / 11
-
+    private fun addCells() {
         for (x in 0..10) {
             for (y in 0..10) {
                 var cell: BoardCellView = if ((x == 0 && y == 0) || (x == 10 && y == 0) ||
@@ -26,11 +34,26 @@ class BoardView : ConstraintLayout {
                 cell.tag = "cell-$x-$y"
                 cell.x = (x * size).toFloat()
                 cell.y = (y * size).toFloat()
-                cells.add(cell)
+                this.addView(cell)
             }
         }
+    }
 
-        cells.forEach { cell -> this.addView(cell)}
+    // TODO: Observer to watch pieces change
+    // TODO: Add King Piece View
+    private fun addPieces(layoutMap: HashMap<Pair<Int, Int>, Piece>) {
+        var attackerCount = 0
+        var defenderCount = 0
+
+        for ((pos, piece) in layoutMap) {
+            var pieceView : PieceView = if (piece.type == PlayerType.ATTACKER) AttackerPieceView(size, context) else DefenderPieceView(size, context)
+            var type = PlayerType.ATTACKER
+
+            pieceView.tag = if (type == PlayerType.ATTACKER) "attacker-${attackerCount++}" else "defender-${defenderCount++}"
+            pieceView.x = (pos.first * size).toFloat()
+            pieceView.y = (pos.second * size).toFloat()
+            this.addView(pieceView)
+        }
     }
 
 }
