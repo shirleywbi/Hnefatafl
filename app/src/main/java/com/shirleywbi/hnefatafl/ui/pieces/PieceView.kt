277d690e20160cf28bce970056bed9a5bf70354b
shirleywbi <swwbi@msn.com>
2020-07-04 12:35:43 -0700

    add piece padding

--- app/src/main/java/com/shirleywbi/hnefatafl/ui/pieces/PieceView.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/ui/pieces/PieceView.kt
@@ -6,11 +6,14 @@ import android.widget.RelativeLayout
 import com.shirleywbi.hnefatafl.R
 
 open class PieceView: androidx.appcompat.widget.AppCompatImageView {
+    private val padding: Int = 8
+
     constructor(size: Int, context: Context) : this(size, context, null)
 
     constructor(size: Int, context: Context, attrs: AttributeSet?) : super(context, attrs) {
         setDimensions(size)
         this.setImageResource(R.drawable.board_cell)
+        this.setPadding(padding, padding, padding, padding)
     }
 
     private fun setDimensions(size: Int) {
