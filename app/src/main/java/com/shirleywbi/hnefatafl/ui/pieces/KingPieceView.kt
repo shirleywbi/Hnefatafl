45e44b588f44f6f40699cbed01fc51ab21a5f23a
shirleywbi <swwbi@msn.com>
2020-07-04 01:49:30 -0700

    add king piece view

--- app/src/main/java/com/shirleywbi/hnefatafl/ui/pieces/KingPieceView.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/ui/pieces/KingPieceView.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.util.AttributeSet
 import com.shirleywbi.hnefatafl.R
 
-open class DefenderPieceView : PieceView {
+class KingPieceView: DefenderPieceView {
 
     constructor(size: Int, context: Context) : this(size, context, null)
 
     constructor(size: Int, context: Context, attrs: AttributeSet?) : super(size, context, attrs) {
         this.setImageResource(R.drawable.piece_defender)
+        // TODO: Differentiate king from defender
     }
+
 }
