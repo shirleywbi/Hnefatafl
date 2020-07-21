d578075408286123c61bd6947ee30183d692d324
shirleywbi <swwbi@msn.com>
2020-07-20 23:47:51 -0700

    add more to how to play, close btn functionality

--- app/src/main/java/com/shirleywbi/hnefatafl/ui/pages/HowToPlayActivity.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/ui/pages/HowToPlayActivity.kt
@@ -3,11 +3,16 @@ package com.shirleywbi.hnefatafl.ui.pages
 import android.app.Activity
 import android.os.Bundle
 import com.shirleywbi.hnefatafl.R
+import kotlinx.android.synthetic.main.howtoplay.*
 
 class HowToPlayActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.howtoplay)
+
+        htp_close_btn.setOnClickListener{
+            finish()
+        }
     }
 }
