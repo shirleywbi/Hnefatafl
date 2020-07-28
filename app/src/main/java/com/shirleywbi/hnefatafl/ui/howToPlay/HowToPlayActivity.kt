147b551f1b5dc7a45a62bd8a6dfe16f790c70002
shirleywbi <swwbi@msn.com>
2020-07-28 00:53:03 -0700

    add dot tab highlighting when selected

--- app/src/main/java/com/shirleywbi/hnefatafl/ui/howToPlay/HowToPlayActivity.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/ui/howToPlay/HowToPlayActivity.kt
@@ -7,7 +7,6 @@ import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.google.android.material.tabs.TabLayout.TabLayoutOnPageChangeListener
 import com.shirleywbi.hnefatafl.R
 import kotlinx.android.synthetic.main.howtoplay.*
-import kotlinx.android.synthetic.main.howtoplayfragment.*
 
 class HowToPlayActivity: AppCompatActivity() {
 
@@ -17,12 +16,17 @@ class HowToPlayActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.howtoplay)
 
+        // Add tab listener
         var listener: OnTabSelectedListener = object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 htp_pager.currentItem = tab.position
+                htp_tab_layout.getTabAt(tab.position)?.setIcon(R.drawable.tab_dot_selected)
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                htp_tab_layout.getTabAt(tab.position)?.setIcon(R.drawable.tab_dot_default)
+            }
+
             override fun onTabReselected(tab: TabLayout.Tab) {}
         }
 
@@ -31,12 +35,17 @@ class HowToPlayActivity: AppCompatActivity() {
             finish()
         }
 
+        // Add tabs
         for (i: Int in 0 until tabCount) {
             htp_tab_layout.addTab(htp_tab_layout.newTab().setIcon(R.drawable.tab_dot_default))
         }
+
+        // Handle page changing
         htp_pager.adapter = HowToPlayPagerAdapter(supportFragmentManager, htp_tab_layout.tabCount)
         htp_pager.addOnPageChangeListener(TabLayoutOnPageChangeListener(htp_tab_layout))
         htp_tab_layout.addOnTabSelectedListener(listener)
+        // TODO: Figure out why this line is not working
+        htp_tab_layout.getTabAt(0)?.select()
     }
 
 }
