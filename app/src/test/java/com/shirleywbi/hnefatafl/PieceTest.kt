ed453a43a1de26f09e2d0246ebbfa3e721935c67
shirleywbi <swwbi@msn.com>
2020-08-08 22:27:15 -0700

    add piece capturing tests

--- app/src/test/java/com/shirleywbi/hnefatafl/PieceTest.kt
+++ app/src/test/java/com/shirleywbi/hnefatafl/PieceTest.kt
@@ -5,8 +5,8 @@ import com.shirleywbi.hnefatafl.service.pieces.ChessPiece
 import com.shirleywbi.hnefatafl.service.pieces.KingPiece
 import com.shirleywbi.hnefatafl.service.pieces.Piece
 import com.shirleywbi.hnefatafl.service.pieces.PieceType
-import junit.framework.TestCase.assertFalse
-import junit.framework.TestCase.assertTrue
+import com.shirleywbi.hnefatafl.service.pieces.PieceType.*
+import junit.framework.TestCase.*
 import org.junit.Before
 import org.junit.Test
 
@@ -22,17 +22,17 @@ class PieceTest {
 
     @Before
     fun init() {
-        atk0x5 = ChessPiece(0, 5, PieceType.ATTACKER)
-        atk5x0 = ChessPiece(5, 0, PieceType.ATTACKER)
-        atk10x3 = ChessPiece(10, 3, PieceType.ATTACKER)
-        def1x2 = ChessPiece(1, 2, PieceType.DEFENDER)
-        def3x2 = ChessPiece(3, 2, PieceType.DEFENDER)
+        atk0x5 = ChessPiece(0, 5, ATTACKER)
+        atk5x0 = ChessPiece(5, 0, ATTACKER)
+        atk10x3 = ChessPiece(10, 3, ATTACKER)
+        def1x2 = ChessPiece(1, 2, DEFENDER)
+        def3x2 = ChessPiece(3, 2, DEFENDER)
 
-        atkBoard = Board(hashMapOf(), PieceType.ATTACKER)
+        atkBoard = Board(hashMapOf(), ATTACKER)
         atkBoard.isAttackerTurn = true
         atkBoard.layoutMap = hashMapOf()
 
-        defBoard = Board(hashMapOf(), PieceType.DEFENDER)
+        defBoard = Board(hashMapOf(), DEFENDER)
         defBoard.isAttackerTurn = false
         defBoard.layoutMap = hashMapOf(
             Pair(1, 2) to def1x2,
@@ -74,60 +74,263 @@ class PieceTest {
 
     @Test
     fun canMove_kingToRestrictedShouldSucceed() {
-        king = KingPiece(0, 2, PieceType.KING)
+        king = KingPiece(0, 2, KING)
         assertTrue(defBoard.canMove(king, 0, 0))
         assertTrue(defBoard.canMove(king, 0, 10))
-        king = KingPiece(10, 2, PieceType.KING)
+        king = KingPiece(10, 2, KING)
         assertTrue(defBoard.canMove(king, 10, 0))
         assertTrue(defBoard.canMove(king, 10, 10))
-        king = KingPiece(5, 4, PieceType.KING)
+        king = KingPiece(5, 4, KING)
         assertTrue(defBoard.canMove(king, 5, 5))
     }
 
     // Capture Tests
 
     @Test
-    fun capture_attackerFlanksDefenderShouldSucceed() {}
+    fun capture_attackerFlanksDefenderShouldSucceed() {
+        assertCapture(ATTACKER, DEFENDER, ATTACKER, atkBoard, true)
+    }
 
     @Test
-    fun capture_defenderFlanksAttackerShouldSucceed() {}
+    fun capture_defenderFlanksAttackerShouldSucceed() {
+        assertCapture(DEFENDER, ATTACKER, DEFENDER, defBoard, true)
+    }
 
     @Test
-    fun capture_attackersOnlyShouldFail() {}
+    fun capture_attackersOnlyShouldFail() {
+        assertCapture(ATTACKER, ATTACKER, ATTACKER, atkBoard, false)
+    }
 
     @Test
-    fun capture_defendersOnlyShouldFail() {}
+    fun capture_defendersOnlyShouldFail() {
+        assertCapture(DEFENDER, DEFENDER, DEFENDER, defBoard, false)
+    }
 
     @Test
-    fun capture_defenderDefenderAttackerShouldFail() {}
+    fun capture_defenderDefenderAttackerShouldFail() {
+        assertCapture(ATTACKER, DEFENDER, DEFENDER, defBoard, false)
+    }
 
     @Test
-    fun capture_attackerAttackerDefenderShouldFail() {}
+    fun capture_attackerAttackerDefenderShouldFail() {
+        assertCapture(DEFENDER, DEFENDER, ATTACKER, atkBoard, false)
+    }
 
     @Test
-    fun capture_attackersSurroundKingShouldSucceed() {}
+    fun capture_attackersFlankKingShouldFail() {
+        assertCapture(ATTACKER, KING, ATTACKER, atkBoard, false)
+    }
 
     @Test
-    fun capture_attackersFlankKingShouldFail() {}
+    fun capture_kingDefenderFlankAttackerShouldSucceed() {
+        assertCapture(DEFENDER, ATTACKER, KING, defBoard, true)
+    }
 
     @Test
-    fun capture_kingDefenderFlankAttackerShouldSucceed() {}
+    fun capture_kingDefenderFlankDefenderShouldFail() {
+        assertCapture(DEFENDER, DEFENDER, KING, defBoard, false)
+    }
 
     @Test
-    fun capture_kingSurroundedOnEdgeLastPieceShouldSucceed() {}
+    fun capture_kingAttackerFlankDefenderShouldFail() {
+        assertCapture(ATTACKER, DEFENDER, KING, defBoard, false)
+    }
 
     @Test
-    fun capture_kingSurroundedOnEdgeNotLastPieceShouldFail() {}
+    fun capture_kingAttackerFlankAttackerShouldFail() {
+        assertCapture(ATTACKER, ATTACKER, KING, defBoard, false)
+    }
+
+    private fun assertCapture(flanker: PieceType, captured: PieceType, moved: PieceType, board: Board, succeed: Boolean) {
+        val piece1 = if (flanker == KING) KingPiece(0, 3, flanker) else ChessPiece(0, 3, flanker)
+        val piece2 = if (captured == KING) KingPiece(1, 3, captured) else ChessPiece(1, 3, captured)
+        val piece3 = if (moved == KING) KingPiece(3, 3, moved) else ChessPiece(3, 3, moved)
+
+        board.layoutMap = hashMapOf(
+            Pair(0, 3) to piece1,
+            Pair(1, 3) to piece2,
+            Pair(3, 3) to piece3
+        )
+
+        assertTrue(board.layoutMap.containsValue(piece2))
+        board.move(piece3, 2, 3)
+        val captures = piece3.capturePositionsAndPieces(2, 3, board.layoutMap)
+        if (succeed) {
+            assertTrue(captures.size == 1)
+            assertTrue(captures.containsValue(piece2))
+            assertFalse(board.layoutMap.containsValue(piece2))
+        } else {
+            assertTrue(captures.size == 0)
+            assertTrue(board.layoutMap.containsValue(piece2))
+        }
+    }
 
     @Test
-    fun capture_kingDefenderFlankDefenderShouldFail() {}
+    fun capture_multiplePieces() {
+        val piece1 = ChessPiece(0, 3, ATTACKER)
+        val piece2 = ChessPiece(1, 3, DEFENDER)
+        val piece3 = ChessPiece(3, 3, ATTACKER)
+        val piece4 = ChessPiece(2, 4, DEFENDER)
+        val piece5 = ChessPiece(2, 5, ATTACKER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(0, 3) to piece1,
+            Pair(1, 3) to piece2,
+            Pair(3, 3) to piece3,
+            Pair(2, 4) to piece4,
+            Pair(2, 5) to piece5
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(piece2))
+        assertTrue(atkBoard.layoutMap.containsValue(piece4))
+        atkBoard.move(piece3, 2, 3)
+        val captures = piece3.capturePositionsAndPieces(2, 3, atkBoard.layoutMap)
+        assertTrue(captures.size == 2)
+        assertTrue(captures.containsValue(piece2))
+        assertTrue(captures.containsValue(piece4))
+        assertFalse(atkBoard.layoutMap.containsValue(piece2))
+        assertFalse(atkBoard.layoutMap.containsValue(piece4))
+    }
 
     @Test
-    fun capture_kingAttackerFlankDefenderShouldFail() {}
+    fun capture_flankedByRestrictedCellShouldSucceed() {
+        val piece1 = ChessPiece(5, 6, ATTACKER)
+        val piece2 = ChessPiece(5, 9, DEFENDER)
+
+        defBoard.layoutMap = hashMapOf(
+            Pair(5, 6) to piece1,
+            Pair(5, 9) to piece2
+        )
+
+        assertTrue(defBoard.layoutMap.containsValue(piece1))
+        defBoard.move(piece2, 5, 7)
+        val captures = piece2.capturePositionsAndPieces(5, 7, defBoard.layoutMap)
+        assertTrue(captures.size == 1)
+        assertTrue(captures.containsValue(piece1))
+        assertFalse(defBoard.layoutMap.containsValue(piece1))
+    }
 
     @Test
-    fun capture_kingAttackerFlankAttackerShouldFail() {}
+    fun capture_flankedByNonEmptyRestrictedCellShouldFail() {
+        val king = KingPiece(5, 5, KING)
+        val piece1 = ChessPiece(5, 6, DEFENDER)
+        val piece2 = ChessPiece(5, 9, ATTACKER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(5, 5) to king,
+            Pair(5, 6) to piece1,
+            Pair(5, 9) to piece2
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(piece1))
+        atkBoard.move(piece2, 5, 7)
+        val captures = piece2.capturePositionsAndPieces(5, 7, atkBoard.layoutMap)
+        assertTrue(captures.isEmpty())
+        assertTrue(atkBoard.layoutMap.containsValue(piece1))
+    }
 
     @Test
-    fun capture_notOnMovementShouldFail() {}
+    fun capture_flankedByEdgeShouldFail() {
+        val piece1 = ChessPiece(0, 1, DEFENDER)
+        val piece2 = ChessPiece(6, 1, ATTACKER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(0, 1) to piece1,
+            Pair(6, 1) to piece2
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(piece1))
+        atkBoard.move(piece2, 1, 1)
+        val captures = piece2.capturePositionsAndPieces(1, 1, atkBoard.layoutMap)
+        assertTrue(captures.isEmpty())
+        assertTrue(atkBoard.layoutMap.containsValue(piece1))
+    }
+
+    @Test
+    fun capture_attackersSurroundKingShouldSucceed() {
+        val king = KingPiece(2, 3, KING)
+        val piece1 = ChessPiece(1, 3, ATTACKER)
+        val piece2 = ChessPiece(3, 3, ATTACKER)
+        val piece3 = ChessPiece(2, 2, ATTACKER)
+        val piece4 = ChessPiece(2, 10, ATTACKER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(2, 3) to king,
+            Pair(1, 3) to piece1,
+            Pair(3, 3) to piece2,
+            Pair(2, 2) to piece3,
+            Pair(2, 10) to piece4
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(king))
+        atkBoard.move(piece4, 2, 4)
+        val captures = piece4.capturePositionsAndPieces(2, 4, atkBoard.layoutMap)
+        assertTrue(captures.size == 1)
+        assertTrue(captures.containsValue(king))
+        assertFalse(atkBoard.layoutMap.containsValue(king))
+    }
+
+    @Test
+    fun capture_kingSurroundedOnEdgeLastPieceShouldSucceed() {
+        val king = KingPiece(0, 5, KING)
+        val piece1 = ChessPiece(4, 4, ATTACKER)
+        val piece2 = ChessPiece(0, 6, ATTACKER)
+        val piece3 = ChessPiece(1, 5, ATTACKER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(0, 5) to king,
+            Pair(4, 4) to piece1,
+            Pair(0, 6) to piece2,
+            Pair(1, 5) to piece3
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(king))
+        atkBoard.move(piece1, 0, 4)
+        val captures = piece1.capturePositionsAndPieces(0, 4, atkBoard.layoutMap)
+        assertTrue(captures.size == 1)
+        assertTrue(captures.containsValue(king))
+        assertFalse(atkBoard.layoutMap.containsValue(king))
+    }
+
+    @Test
+    fun capture_kingSurroundedOnEdgeNotLastPieceShouldFail() {
+        val king = KingPiece(0, 5, KING)
+        val piece1 = ChessPiece(4, 4, ATTACKER)
+        val piece2 = ChessPiece(0, 6, ATTACKER)
+        val piece3 = ChessPiece(1, 5, ATTACKER)
+        val defender = ChessPiece(8, 8, DEFENDER)
+
+        atkBoard.layoutMap = hashMapOf(
+            Pair(0, 5) to king,
+            Pair(4, 4) to piece1,
+            Pair(0, 6) to piece2,
+            Pair(1, 5) to piece3,
+            Pair(8, 8) to defender
+        )
+
+        assertTrue(atkBoard.layoutMap.containsValue(king))
+        atkBoard.move(piece1, 0, 4)
+        val captures = piece1.capturePositionsAndPieces(0, 4, atkBoard.layoutMap)
+        assertTrue(captures.isEmpty())
+        assertTrue(atkBoard.layoutMap.containsValue(king))
+    }
+
+    @Test
+    fun capture_notOnMovementShouldFail() {
+        val piece1 = ChessPiece(0, 3, ATTACKER)
+        val piece2 = ChessPiece(1, 9, DEFENDER)
+        val piece3 = ChessPiece(2, 3, ATTACKER)
+
+        defBoard.layoutMap = hashMapOf(
+            Pair(0, 3) to piece1,
+            Pair(1, 9) to piece2,
+            Pair(2, 3) to piece3
+        )
+
+        assertTrue(defBoard.layoutMap.containsValue(piece2))
+        defBoard.move(piece2, 1, 3)
+        val captures = piece2.capturePositionsAndPieces(1, 3, defBoard.layoutMap)
+        assertTrue(captures.isEmpty())
+        assertTrue(defBoard.layoutMap.containsValue(piece2))
+    }
 }
