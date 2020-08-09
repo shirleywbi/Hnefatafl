cac321dcc4794d4c30a39333e9837183b8cd664d
shirleywbi <swwbi@msn.com>
2020-08-08 17:12:42 -0700

    refactor piece setup from board to game

--- app/src/main/java/com/shirleywbi/hnefatafl/service/Board.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/service/Board.kt
@@ -14,12 +14,12 @@ class Board: Serializable {
     lateinit var gameOverStatus: GameOverStatus
     var isAttackerTurn = true
 
-    var layoutMap: HashMap<Pair<Int, Int>, Piece> = hashMapOf()
+    var layoutMap: HashMap<Pair<Int, Int>, Piece>
     var boardHistory: HashMap<HashMap<Pair<Int, Int>, Piece>, Int> = hashMapOf()
     lateinit var playerType: PieceType
 
-    constructor() {
-        setupPieces()
+    constructor(layoutMap: HashMap<Pair<Int, Int>, Piece>) {
+        this.layoutMap = layoutMap
     }
 
     // Alternate turns to determine whether piece can move, starting with attacker
@@ -101,32 +101,4 @@ class Board: Serializable {
         return true
     }
 
-    private fun setupPieces() {
-        var attackerCount = 0
-        var defenderCount = 0
-        // set up attackers
-        for (x in 0..10) {
-            if (x in 3..7) {
-                layoutMap[Pair(x, 0)] = ChessPiece(x, 0, PieceType.ATTACKER, "attacker-${attackerCount++}")
-                layoutMap[Pair(x, 10)] = ChessPiece(x, 10, PieceType.ATTACKER, "attacker-${attackerCount++}")
-            }
-            if (x == 5) {
-                layoutMap[Pair(x, 1)] = ChessPiece(x, 1, PieceType.ATTACKER, "attacker-${attackerCount++}")
-                layoutMap[Pair(x, 9)] = ChessPiece(x, 9, PieceType.ATTACKER, "attacker-${attackerCount++}")
-            }
-            if (x == 0 || x == 10) for (y in 3..7) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.ATTACKER, "attacker-${attackerCount++}")
-            if (x == 1 || x == 9) layoutMap[Pair(x, 5)] = ChessPiece(x, 5, PieceType.ATTACKER, "attacker-${attackerCount++}")
-        }
-
-        // set up defenders
-        for (x in 3..7) {
-            if (x == 3 || x == 7) layoutMap[Pair(x, 5)] = ChessPiece(x, 5, PieceType.DEFENDER, "defender-${defenderCount++}")
-            if (x == 4 || x == 6) for (y in 4..6) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
-            if (x == 5) {
-                for (y in 3..4) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
-                layoutMap[Pair(5, 5)] = KingPiece(5, 5)
-                for (y in 6..7) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
-            }
-        }
-    }
 }
--- app/src/main/java/com/shirleywbi/hnefatafl/service/Game.kt
+++ app/src/main/java/com/shirleywbi/hnefatafl/service/Game.kt
@@ -1,16 +1,51 @@
 package com.shirleywbi.hnefatafl.service
 
+import com.shirleywbi.hnefatafl.service.pieces.ChessPiece
+import com.shirleywbi.hnefatafl.service.pieces.KingPiece
+import com.shirleywbi.hnefatafl.service.pieces.Piece
 import com.shirleywbi.hnefatafl.service.pieces.PieceType
 import java.io.Serializable
 
 class Game: Serializable {
 
     lateinit var piece: PieceType
-    var gameBoard: Board = Board()
+    lateinit var gameBoard: Board
 
     fun selectPlayer(type: PieceType) {
         piece = type
+        gameBoard = Board(generatePieceLayout())
         gameBoard.playerType = type
     }
 
+    private fun generatePieceLayout(): HashMap<Pair<Int, Int>, Piece> {
+        var layoutMap: HashMap<Pair<Int, Int>, Piece> = hashMapOf()
+        var attackerCount = 0
+        var defenderCount = 0
+
+        // set up attackers
+        for (x in 0..10) {
+            if (x in 3..7) {
+                layoutMap[Pair(x, 0)] = ChessPiece(x, 0, PieceType.ATTACKER, "attacker-${attackerCount++}")
+                layoutMap[Pair(x, 10)] = ChessPiece(x, 10, PieceType.ATTACKER, "attacker-${attackerCount++}")
+            }
+            if (x == 5) {
+                layoutMap[Pair(x, 1)] = ChessPiece(x, 1, PieceType.ATTACKER, "attacker-${attackerCount++}")
+                layoutMap[Pair(x, 9)] = ChessPiece(x, 9, PieceType.ATTACKER, "attacker-${attackerCount++}")
+            }
+            if (x == 0 || x == 10) for (y in 3..7) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.ATTACKER, "attacker-${attackerCount++}")
+            if (x == 1 || x == 9) layoutMap[Pair(x, 5)] = ChessPiece(x, 5, PieceType.ATTACKER, "attacker-${attackerCount++}")
+        }
+
+        // set up defenders
+        for (x in 3..7) {
+            if (x == 3 || x == 7) layoutMap[Pair(x, 5)] = ChessPiece(x, 5, PieceType.DEFENDER, "defender-${defenderCount++}")
+            if (x == 4 || x == 6) for (y in 4..6) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
+            if (x == 5) {
+                for (y in 3..4) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
+                layoutMap[Pair(5, 5)] = KingPiece(5, 5)
+                for (y in 6..7) layoutMap[Pair(x, y)] = ChessPiece(x, y, PieceType.DEFENDER, "defender-${defenderCount++}")
+            }
+        }
+        return layoutMap
+    }
 }
